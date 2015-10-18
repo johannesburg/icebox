966a2a1d9556e3c08fef18b317478c1f37b8fe8a
Narbulus <s.hughes.anderson@gmail.com>
2015-10-18 00:49:05 -0700

    Working firebase stuff

--- app/public/populateList.js
+++ app/public/populateList.js
@@ -1,21 +1,37 @@
-var myDataRef = new Firebase('https://mo9gnntyjxw.firebaseio-demo.com/');
-// When we press enter
-$('#itemInput').keypress(function (e) {
-  if (e.keyCode == 13) {
-    var name = $('#itemInput').val();
-    myDataRef.push({itemName: name});
-    $('#itemInput').val('');
-  }
-});
-myDataRef.on('child_added', function(snapshot) {
-   var newItem = snapshot.val();
-   addNewItem(newItem.itemName);
-});
+function start() {
+  var myDataRef = new Firebase("https://boiling-inferno-5486.firebaseio.com/");
+  // When we press enter
+  
+  var input = document.getElementById("itemInput");
+
+  input.onkeypress = function (e) {
+    if (e.keyCode == 13) {
+      var name = input.value;
+      myDataRef.push({"itemName": name});
+      e.preventDefault();
+      return false;
+    }
+  };
+  
+  myDataRef.on('child_added', function(snapshot) {
+     var newItem = snapshot.val();
+     addNewItem(newItem.itemName);
+  });
+}
+
 function addNewItem(name) {
-  var newEntry = document.createElemtn("p");
+  var newEntry = document.createElement("div");
+
   var text = document.createTextNode(name);
   newEntry.appendChild(text);
 
   var parent = document.getElementById("itemTable");
   parent.appendChild(newEntry);
 };
+
+function $(tag) {
+  return document.getElementById(tag);
+}
+
+
+window.onload = start;
