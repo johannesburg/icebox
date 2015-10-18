eb2749d7772855b5b198a335ead52a541b2918ad
Narbulus <s.hughes.anderson@gmail.com>
2015-10-18 05:03:29 -0700

    Centered button

--- app/public/populateList.js
+++ app/public/populateList.js
@@ -71,7 +71,7 @@ function uploadFile(e) {
   reader.onload = (function(theFile) {
     return function(e) {
       var filePayload = e.target.result;
-      var f = new Firebase(basePath + '/imageLoading/' + uid + "/");
+      var f = new Firebase(basePath + '/imageLoading/');
       // Set the file payload to Firebase and register an onComplete handler to stop the spinner and show the preview
       f.set(filePayload, function() { 
         console.log("upload complete");
