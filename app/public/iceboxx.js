210a1ef6c0098bae800e3b93218b040c940941b8
Narbulus <s.hughes.anderson@gmail.com>
2015-10-18 14:10:19 -0700

    Whitelist

--- app/public/iceboxx.js
+++ app/public/iceboxx.js
@@ -2,6 +2,9 @@ var ref;
 var userRef;
 var basePath = "https://boiling-inferno-5486.firebaseio.com/";
 var uid;
+var tagSelect = [];
+var tagIgnore = [];
+var lastURL = "";
 
 var imgurClientID = "a7bc24269b5f209";
 var imgurSecret = "5d5507dd629d18759ff61af04148513997cce638";
@@ -50,16 +53,16 @@ function onAuth(authData) {
   input.onkeypress = function (e) {
     if (e.keyCode == 13) {
       e.preventDefault();
-      var name = input.value;
-      userRef.push({"itemName": name, "itemCount": 1});
+      tryAddInput(input, userRef);
       return false;
     }
   };
 
+  document.getElementById("submitTags").onclick = submitTags;
+
   var submitButton = document.getElementById("submitItem");
   submitButton.onclick = function (e) {
-    var name = input.value;
-    userRef.push({"itemName": name, "itemCount": 1});
+    tryAddInput(input, userRef);
   }
 
   document.getElementById("logout").onclick = function (e) {
@@ -75,6 +78,18 @@ function onAuth(authData) {
   userRef.on('child_removed', function(snapshot) {showDBSpinner();});
 }
 
+function tryAddInput(input, userRef) {
+  var name = input.value;
+  if (name.trim().length > 0) {
+    if (!lastURL) {
+      userRef.push({"itemName": name, "itemCount": 1});
+    } else {
+      addNewTag(name, true);
+    }
+    input.value = "";
+  }
+}
+
 function showDBSpinner() {
   var spinner = document.getElementById("updateDBSpinner");
   spinner.style.display = "inline-block";
@@ -87,21 +102,39 @@ function showTags(tagList) {
   var tagContainer = document.getElementById("tagList");
   tagContainer.innerHTML = "";
   for (index = 0; index < tagList.length; index++) {
-    var newTag = document.createElement("a");
-    newTag.onclick = function(e) {
-      var tar = e.target;
-      if (tar.className != "btn button-padding btn-success") {
-        tar.className = "btn button-padding btn-success";
-      } else {
-        tar.className = "btn button-padding btn-default";
-      }
-    };
-    var newContent = document.createTextNode(tagList[index]);
-    newTag.className = "btn button-padding btn-default";
-    newTag.href = "#";
-    newTag.appendChild(newContent);
-    tagContainer.appendChild(newTag);
+    addNewTag(tagList[index], false);
   }
+}
+
+function addNewTag(tagName, selected) {
+  var tagContainer = document.getElementById("tagList");
+  var newTag = document.createElement("a");
+  if (selected) {
+    addTagToWhitelist(tagName);
+    tagSelect.push(tagName);
+  } else {
+    tagIgnore.push(tagName);
+  }
+  newTag.onclick = function(e) {
+    var tar = e.target;
+    var value = e.target.innerHTML;
+    if (tar.className != "btn button-padding btn-success") {
+      tar.className = "btn button-padding btn-success";
+      tagSelect.push(value);
+      tagIgnore.splice(tagIgnore.indexOf(value), 1);
+    } else {
+      tar.className = "btn button-padding btn-default";
+      tagIgnore.push(value);
+      tagSelect.splice(tagSelect.indexOf(value), 1);
+    }
+    console.log(e.target.innerHTML);
+  };
+  var newContent = document.createTextNode(tagName);
+  newTag.className = "btn button-padding btn-default";
+  if (selected)
+    newTag.className = "btn button-padding btn-success";
+  newTag.appendChild(newContent);
+  tagContainer.appendChild(newTag);
 }
 
 function cameraClick(e) {
@@ -109,7 +142,9 @@ function cameraClick(e) {
 }
 
 function fileSelected(e) {
-  console.log("Loading files")
+  console.log("Loading files");
+  var spinner = document.getElementById("updateDBSpinner");
+  spinner.style.display = "inline-block";
   document.getElementById("loadingIcon").style.display = "block";
   var count = document.getElementById('fileToUpload').files.length;
   for (var index = 0; index < count; index ++) {
@@ -149,7 +184,7 @@ function fileSelected(e) {
    http.send(fd);
 }
 
-function showImage(file,target) {
+function showImage(file, target) {
   var fr=new FileReader();
   // when image is loaded, set the src of the image where you want to display it
   fr.onload = function(e) { target.src = this.result; };
@@ -159,12 +194,33 @@ function showImage(file,target) {
 
 function onTags(success, url, tags) {
   if (success) {
+    lastURL = url;
+    document.getElementById("updateDBSpinner").style.display = "none";
+    document.getElementById("tagListDiv").style.display = "block";
     showTags(tags);
   }
 }
 
 function submitTags() {
+  var i;
+  console.log(tagSelect);
+  for (i = 0; i < tagSelect.length; i++) {
+      userRef.push({"itemName": tagSelect[i], "itemCount": 1});
+  }
+  var tagContainer = document.getElementById("tagList");
+  tagContainer.innerHTML = "";
+  giveFeedback(lastURL, tagSelect, tagIgnore);
+  // Clear old info
+  console.println("Good: " + tagSelect.toString());
+  console.println("Bad: " + tagIgnore.toString());
+  lastURL = "";
+  tagSelect = [];
+  tagIgnore = [];
 
+  document.getElementById("imageInput").style.display = "block";
+  document.getElementById("imageOutputDiv").style.display = "none";
+  document.getElementById("tagListDiv").style.display = "none";
+  document.getElementById("imageOutput").innerHTML = "";
 }
 
 function addNewItem(accessRef) {
@@ -195,13 +251,13 @@ function addNewItem(accessRef) {
 
   var deleteButton = document.createElement("td");
   deleteButton.className = "col-sm-5";
-  var deleteIcon = document.createElement("i");
+  var deleteIcon = document.createElement("button");
   deleteIcon.onclick = function(e) {
     var row = document.getElementById(name);
     row.parentNode.removeChild(row);
     userRef.child(accessRef.key()).remove();
   };
-  deleteIcon.className = "fa fa-times fa-2x";
+  deleteIcon.innerHTML = "<i style=\"color: #990033;\" class=\"fa fa-times fa-2x\" />"
   deleteButton.appendChild(deleteIcon);
   row.appendChild(deleteButton);
 
