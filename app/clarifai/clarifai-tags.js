a31991e426228d1befdeae3699bcda4246254220
bgardon <bgardon@cs.washington.edu>
2015-10-18 02:33:16 -0700

    improved get tags

--- app/clarifai/clarifai-tags.js
+++ app/clarifai/clarifai-tags.js
@@ -14,22 +14,33 @@ Clarifai.setThrottleHandler( function( bThrottled, waitSeconds ) {
 	console.log( bThrottled ? ["throttled. service available again in",waitSeconds,"seconds"].join(' ') : "not throttled");
 });
 
+function filterTags(tags) {
+	var filteredTags = tags;
+	//filter tags
+	return filteredTags;
+}
+
 function commonResultHandler(err, res, resultsCallback) {
 	if( err != null ) {
 		if( typeof err["status_code"] === "string" && err["status_code"] === "TIMEOUT") {
-			resultsCallback(false, "TAG request timed out");
+			resultsCallback(false, "", "", "");
+			console.log("TAG request timed out");
 		}
 		else if( typeof err["status_code"] === "string" && err["status_code"] === "ALL_ERROR") {
-			resultsCallback(false, "TAG request received ALL_ERROR. Contact Clarifai support if it continues.");				
+			resultsCallback(false, "", "", "");
+			console.log("TAG request received ALL_ERROR. Contact Clarifai support if it continues.");				
 		}
 		else if( typeof err["status_code"] === "string" && err["status_code"] === "TOKEN_FAILURE") {
-			resultsCallback(false, "TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");				
+			resultsCallback(false, "", "", "");
+			console.log("TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");				
 		}
 		else if( typeof err["status_code"] === "string" && err["status_code"] === "ERROR_THROTTLED") {
-			resultsCallback(false, "Clarifai host is throttling this application.");				
+			resultsCallback(false, "", "", "");
+			console.log("Clarifai host is throttling this application.");				
 		}
 		else {
-			resultsCallback(false, "TAG request encountered an unexpected error." + err);		
+			resultsCallback(false, "", "", "");
+			console.log("TAG request encountered an unexpected error." + err);		
 		}
 	}
 	else {
@@ -44,17 +55,16 @@ function commonResultHandler(err, res, resultsCallback) {
 			// the request completed successfully
 			for( i = 0; i < res.results.length; i++ ) {
 				if( res["results"][i]["status_code"] === "OK" ) {
-					resultsCallback(true, 
-						'docid='+res.results[i].docid +
-						' local_id='+res.results[i].local_id +
-						' tags='+res["results"][i].result["tag"]["classes"] )
+					resultsCallback(true,
+						res.results[i].local_id,
+						res.results[i].docid,
+						filterTags(res["results"][i].result["tag"]["classes"]))
 				}
 				else {
-					resultsCallback(true, 
-						'docid='+res.results[i].docid +
-						' local_id='+res.results[i].local_id + 
-						' status_code='+res.results[i].status_code +
-						' error = '+res.results[i]["result"]["error"] )
+					resultsCallback(false,
+						res.results[i].local_id,
+						res.results[i].docid,
+						"")
 				}
 			}
 		}			
@@ -63,23 +73,25 @@ function commonResultHandler(err, res, resultsCallback) {
 
 
 // Takes a url for a picture and a local ID for the url.
-// Calls resultsCallback which whould be a function which takes a boolean indicating sucess and a string. If the request
-// succeeded then the string will contain the result, otherwise an error message.
+// Calls resultsCallback which should be a function which takes a boolean representing success, a local id string, a doc id, and an array of tags
 function tagURL(URL, localId, resultCallback) {
 	Clarifai.tagURL( URL , localId, function (errors, results) {commonResultHandler(errors, results, resultCallback)}); 
 }
 
-// Takes an array of urls for pictures and an array of local IDs for each url. The two arrays must be of the same length.
-// Calls resultsCallback which whould be a function which takes a boolean indicating sucess and a string. If the request
-// succeeded then the string will contain the result, otherwise an error message.
+// Takes an array of urls for pictures and an array of local IDs for each url.
+// Calls resultsCallback multiple times. It should be a function which takes a boolean representing success, a local id string, a doc id, and an array of tags
 function tagURLs(URLs, localIds, resultsCallback) {
-	if (URLs.length != localIds.length)
-		resultsCallback(false, "ERROR - list of URLs and local IDs do not have the same length!");
-
-
 	Clarifai.tagURL( URLs , localIds, function (errors, results) {commonResultHandler(errors, results, resultsCallback)}); 
 }
 
-tagURL("http://www.travelingwellforless.com/wp-content/uploads/2016/01/groceries.jpg", "canadian groceries", function (success, message) {console.log(message)})
+tagURL("http://www.travelingwellforless.com/wp-content/uploads/2016/01/groceries.jpg",
+	"canadian groceries", 
+	function (success, localId, docId, tags) {
+		if (!success)
+			console.log("error: " + localId);
+		else {
+			console.log(localId);
+			console.log(tags);
+		}});
 
 Clarifai.clearThrottleHandler();
