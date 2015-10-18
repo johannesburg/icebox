c006194bb75b2a5e7ccd67bf55ed1a7018beab22
bgardon <bgardon@cs.washington.edu>
2015-10-17 23:33:30 -0700

    added clarifai api files

---
+++ app/clarifai/clarifai-wrapper.js
@@ -0,0 +1,6 @@
+process.env['CLARIFAI_CLIENT_ID'] = '34EZ1WNwGt7dvL08d-k2BNfutb-ZqqOh8mmdQXNP';
+process.env['CLARIFAI_CLIENT_SECRET'] = 'QTU11PsOato83dZz5z_pxzbCapAQbtNAyMeaigIW';
+
+var Clarifai = require('./clarifai_node.js');
+
+Clarifai.initAPI(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
