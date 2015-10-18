f46609e5acdc4c8dad18334b85069a461716519b
Johan Michalove <johan@flexminder.com>
2015-10-17 21:45:35 -0700

    kill views code

--- app/app.js
+++ app/app.js
@@ -14,10 +14,6 @@ var myFirebaseRef = new Firebase("https://boiling-inferno-5486.firebaseio.com/")
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
